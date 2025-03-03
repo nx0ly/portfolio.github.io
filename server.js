9de65e5a28e666d80c95fe2c1e5ac1d357214dee
nx0ly <n9d8sa@gmail.com>
2025-03-03 17:44:44 -0500

    update

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require("express");
 const path = require("node:path")
 
 const app = express();
+require("dotenv").config();
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "./public/html/main.html"));
+app.use(express.static("public/view", {
+	extensions: ["html"]
+}));
+
+app.get("/panel", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/admin/panel.html"));
 });
 
-app.listen(7801, () => {
-	console.log(`listening on port ${7801}`);
-	console.log("OK");
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, req.url));
+	console.log(req.url);
+})
+
+app.listen(process.env.PORT, () => {
+	console.log(`listening on port ${process.env.PORT}`);
 });
